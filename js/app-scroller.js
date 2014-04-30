04f84c558f64fb4158012aa377a66cd556168583
Jeppe Bårris <jeppe@redpitt.com>
2014-04-30 11:47:27 +0200

    Scroll Test 671
    Removed all animations. Added a 70% transparent png for the overlay bg.

--- js/app-scroller.js
+++ js/app-scroller.js
@@ -1,50 +1,22 @@
 $(function() {
 	
   FastClick.attach(document.body);
-
-  var rowno = 3;
-	var colno = 4;
-	var stageW = $(window).width()
-	var stageH = $(window).height()
-	var unitW = 480; //  stageW / colno;
-	var unitH = 355; // stageH / rowno;
-	
-  // $('.img').width(unitW).height(unitH);
-	
-  // window.mySwipe = $('.mySwipe').show().width(stageW).height(unitH).Swipe().data('Swipe');
-	// $.nonbounce();
-
  
   // Click functions ...
-  
-
     $( ".img" ).click(function() {
-      // alert('CLICK!');
       var $dataInfoFile = $(this).data("infofile");
       var fileUrl = 'info/'+ $dataInfoFile +'.htm';
 
       $.get( fileUrl, function( data ) {
         $( "#info-box" ).html( data );
-        //$('#overlay').fadeIn('fast',function(){$('#overlay-box').animate({'top':'160px'},500);});
-        $('#overlay').fadeIn('fast',function(){$('#overlay-box').animate({'top':'160px'},300);});
-        // $('#overlay-box').show();
-        //$('#overlay-box').animate({'top':'160px'},500);
+        $('#overlay').css({'display':'block'});
+        $('#overlay-box').css({'top':'160px'})
 
         $('#overlay').click(function() { 
-
-            $('#overlay-box').animate({'top':'-200px'},300,function(){ $('#overlay').fadeOut('fast'); });
-            
+            $('#overlay').css({'display':'none'});
+            $( "#info-box" ).html( '' );
+            $('#overlay-box').css({'top':'-200px'});
         });
-
-
-       
       });
     }); // end img.click(function)
-  
-
-    
-
-  // $('#boxclose').click(function(){
-  //   $('#overlay-box').animate({'top':'-200px'},300,function(){ $('#overlay').fadeOut('fast'); });
-  // });
 });
