44132ad42a4df925e290df853b91e960ea8a701c
Jeppe Bårris <jeppe@redpitt.com>
2014-04-30 13:28:54 +0200

    Preload and jquery mobile
    Preloading info page
    Added jquery mobile

--- js/app-scroller.js
+++ js/app-scroller.js
@@ -1,18 +1,22 @@
 $(function() {
+
+  // Cache info pages ....
+  var fileUrl = 'info/xxxxxxx.htm';
+  var info_xxxxxxx = '';
+  $.get( fileUrl, function( data ) {
+    info_xxxxxxx = data;
+  });
 	
   FastClick.attach(document.body);
  
   // Click functions ...
     $( ".img" ).click(function() {
       var $dataInfoFile = $(this).data("infofile");
-      var fileUrl = 'info/'+ $dataInfoFile +'.htm';
+
       $( "#info-box" ).html( '' );
       $('#overlay').css({'display':'block'});
-      $('#overlay-box').css({'top':'160px'})
-
-      $.get( fileUrl, function( data ) {
-        $( "#info-box" ).html( data );
-      });
+      $('#overlay-box').css({'top':'160px'});
+      $('#overlay-box').html(info_xxxxxxx);
 
       $('#overlay').click(function() { 
           $('#overlay').css({'display':'none'});
