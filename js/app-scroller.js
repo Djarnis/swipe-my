fc4dd39a3e3c62f451f86689efe71919af4d49d3
Jeppe Bårris <jeppe@redpitt.com>
2014-04-30 13:58:26 +0200

    Optimized js

--- js/app-scroller.js
+++ js/app-scroller.js
@@ -1,28 +1,37 @@
-$(function() {
+var info_xxxxxxx = '';
 
+$(function() {
+	
+  FastClick.attach(document.body);
+  preCache();
+  preClick();
+  
+});
+
+function preClick(){
+  // Click functions ...
+  $('.img').click(function() {
+    showInfo( $(this).data("infofile") );
+  });
+}
+
+function preCache(){
   // Cache info pages ....
   var fileUrl = 'info/xxxxxxx.htm';
-  var info_xxxxxxx = '';
   $.get( fileUrl, function( data ) {
     info_xxxxxxx = data;
   });
-	
-  FastClick.attach(document.body);
- 
-  // Click functions ...
-    $( ".img" ).click(function() {
-      var $dataInfoFile = $(this).data("infofile");
+}
 
-      $( "#info-box" ).html( '' );
-      $('#overlay').css({'display':'block'});
-      $('#overlay-box').css({'top':'160px'});
-      $('#overlay-box').html(info_xxxxxxx);
-
-      $('#overlay').click(function() { 
-          $('#overlay').css({'display':'none'});
-          $( "#info-box" ).html( '' );
-          $('#overlay-box').css({'top':'-200px'});
-      });
-
-    }); // end img.click(function)
-});
+function showInfo( strInfoFile ){
+  $( "#info-box" ).html( '' );
+  $('#overlay').css({'display':'block'});
+  $('#overlay-box').css({'top':'160px'});
+  $('#overlay-box').html(info_xxxxxxx);
+  $('#overlay').click( hideInfo );
+}
+function hideInfo(){
+  $('#overlay').css({'display':'none'});
+  $( "#info-box" ).html( '' );
+  $('#overlay-box').css({'top':'-200px'});
+}
