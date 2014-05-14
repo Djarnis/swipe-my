d9970c8c7069a74c96958290c1f38eb505b0dee8
Jeppe Bårris <jeppe@redpitt.com>
2014-05-14 18:04:45 +0200

    New img + stuff

--- js/jquery.appscroller.js
+++ js/jquery.appscroller.js
@@ -6,14 +6,40 @@ $(function() {
   preClick();
   
   FastClick.attach(document.body);
+
+  /* 
+  $('.load-row-1').waypoint(function(direction) {
+    alert('Top of thing hit top of viewport.');
+  });
+  */
+
+ /* 
+
+  $('.load-row-1').waypoint(function(direction) {
+    alert('Top of thing hit top of viewport.');
+  });
+
+
+
+  $('.infinite-container').waypoint('infinite', {
+  container: 'auto',
+  items: '.infinite-item',
+  more: '.infinite-more-link',
+  offset: 'bottom-in-view',
+  loadingClass: 'infinite-loading',
+  onBeforePageLoad: $.noop,
+  onAfterPageLoad: $.noop
+});
   
+
+  */
   
 });
 
 function preClick(){
   // Click functions ...
   $('.img').click(function() {
-    showInfo( $(this).data("infofile") );
+    showInfo( $(this).data("infofile"), $(this).data("infotype") );
   });
 }
 
@@ -25,15 +51,69 @@ function preCache(){
   });
 }
 
-function showInfo( strInfoFile ){
-  $( "#info-box" ).html( '' );
+function getPage(strSlug){
+  var fileUrl = 'info/'+ strSlug +'.htm';
+  var rtn = '';
+   $.get( fileUrl, function( data ) {
+    rtn = data;
+  });
+
+
+   return rtn;
+}
+
+function showInfo( strInfoFile, infoType ){
+  $( ".info-box" ).html( '' );
+  $('.overlay-box').removeClass('info-box-image');
+  
   $('#overlay').css({'display':'block'});
-  $('#overlay-box').css({'top':'160px'});
-  $('#overlay-box').html(info_xxxxxxx);
+  /* $('.overlay-box').css({'top':'160px'}); */
+  
+  
   $('#overlay').click( hideInfo );
+  $('.overlay-box').click( hideInfo );
+
+  var infohtml = '';
+
+  // Get the info
+  if(infoType == 'htm'){
+    
+
+    var fileUrl = 'info/'+ strInfoFile +'.htm';
+    
+     $.get( fileUrl, function( data ) {
+      infohtml = data;
+      // $('.info-box').html('strInfoFile: '+ strInfoFile +'<br>infoType: '+ infoType + '<br>'+ infohtml);
+      $('.info-box').html(infohtml);
+    });
+
+
+  } else {
+    // image?
+    infohtml = '<img src="info/'+ strInfoFile +'.'+ infoType +'" width="1920" height="1080" id="infoimg" />';
+
+
+    // $('.overlay-box').addClass('info-box-image');
+
+
+    // $('.overlay-box').html('strInfoFile: '+ strInfoFile +'<br>infoType: '+ infoType + '<br>'+ infohtml);
+    $('.info-box').html(infohtml);
+    $('.overlay-box').addClass('info-box-image');
+    // $('.overlay-box').css({'top':'0'});
+    // $('.overlay-box').css({'left':'0'});
+
+    $('#infoimg').click( hideInfo );
+  }
+
+  $( ".overlay-box" ).show();
+  
+
 }
 function hideInfo(){
+  $('.overlay-box').removeClass('info-box-image');
   $('#overlay').css({'display':'none'});
-  $( "#info-box" ).html( '' );
-  $('#overlay-box').css({'top':'-200px'});
+  $( ".info-box" ).html( '' );
+  $( ".overlay-box" ).html( '<a class="boxclose" id="boxclose"></a><div class="info-box"></div>' );
+  $( ".overlay-box" ).hide();
+  /* $('.overlay-box').css({'top':'-200px'}); */
 }
